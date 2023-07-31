95dbf580877c9f902c21465b3bf352b66f7eac33
rafin8990 <rafin8990@gmail.com>
2023-07-31 20:43:08 +0600

    download code added

--- Script/download.js
+++ Script/download.js
@@ -1,11 +1,42 @@
-document.getElementById("btn-download").addEventListener("click", () => {
-  const imageContent = document.getElementById("download-content");
-  const anchorTag = document.createElement("a");
-  html2canvas(imageContent).then((canvas) => {
-    const imageUrl = canvas.toDataURL("image/png");
-    anchorTag.setAttribute("href", imageUrl);
-    anchorTag.setAttribute("download", "posterImage.png");
-    anchorTag.click();
-    anchorTag.remove();
-  });
-});
+// const downloadButton = document.getElementById("btn-download");
+// downloadButton.addEventListener("click", () => {
+//   const divToDownload = document.getElementById("download-content");
+
+//   const computedStyle = window.getComputedStyle(divToDownload);
+//   const width = parseInt(computedStyle.getPropertyValue("width"));
+//   const height = parseInt(computedStyle.getPropertyValue("height"));
+//   const canvas = document.createElement("canvas");
+
+//   canvas.width = width;
+//   canvas.height = height;
+//   const context = canvas.getContext("2d");
+
+//   // Draw the contents of the div on the canvas
+//   const imageData = new Image();
+//   const svgData = new XMLSerializer().serializeToString(divToDownload);
+//   const svgBlob = new Blob([svgData], { type: "image/svg+xml" });
+//   const url = URL.createObjectURL(svgBlob);
+
+//   imageData.onload = function () {
+//     context.drawImage(imageData, 0, 0, width, height);
+
+//     // Convert canvas data to PNG and create a File object
+//     canvas.toBlob(function (blob) {
+//       const file = new File([blob], "Poster.png", { type: "image/png" });
+
+//       // Create a download link for the PNG image
+//       const downloadLink = document.createElement("a");
+//       downloadLink.href = URL.createObjectURL(file);
+//       downloadLink.download = "Poster.png";
+
+//       // Trigger the download
+//       downloadLink.click();
+
+//       // Release the object URLs
+//       URL.revokeObjectURL(url);
+//       URL.revokeObjectURL(downloadLink.href);
+//     }, "image/png");
+//   };
+//   console.log("clicked");
+//   imageData.src = url;
+// });
