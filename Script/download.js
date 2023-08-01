02c7d7ef1a3b6f058bc27d1cdd04cad5e6c8cacd
rafin8990 <rafin8990@gmail.com>
2023-08-01 17:23:54 +0600

    download code added

--- Script/download.js
+++ Script/download.js
@@ -1,42 +1,29 @@
-// const downloadButton = document.getElementById("btn-download");
-// downloadButton.addEventListener("click", () => {
-//   const divToDownload = document.getElementById("download-content");
+const downloadButton = document.getElementById("btn-download");
+downloadButton.addEventListener("click", () => {
+  const pageContent = document.getElementById("download-content");
 
-//   const computedStyle = window.getComputedStyle(divToDownload);
-//   const width = parseInt(computedStyle.getPropertyValue("width"));
-//   const height = parseInt(computedStyle.getPropertyValue("height"));
-//   const canvas = document.createElement("canvas");
+  const canvas = document.createElement("canvas");
+  canvas.width = pageContent.scrollWidth;
+  canvas.height = pageContent.scrollHeight;
+  const context = canvas.getContext("2d");
 
-//   canvas.width = width;
-//   canvas.height = height;
-//   const context = canvas.getContext("2d");
+  // Draw the contents of the div on the canvas
+  //   const imageData = new Image();
+  const img = document.getElementById("img");
+  const svgData = new XMLSerializer().serializeToString(pageContent);
+  const svgBlob = new Blob([svgData], {
+    type: "data:image/svg+xml;charset=utf-8",
+  });
+  const url = URL.createObjectURL(svgBlob);
+  img.src = url;
+  console.log(url);
+  context.drawImage(img, 0, 0, canvas.width, canvas.height);
 
-//   // Draw the contents of the div on the canvas
-//   const imageData = new Image();
-//   const svgData = new XMLSerializer().serializeToString(divToDownload);
-//   const svgBlob = new Blob([svgData], { type: "image/svg+xml" });
-//   const url = URL.createObjectURL(svgBlob);
-
-//   imageData.onload = function () {
-//     context.drawImage(imageData, 0, 0, width, height);
-
-//     // Convert canvas data to PNG and create a File object
-//     canvas.toBlob(function (blob) {
-//       const file = new File([blob], "Poster.png", { type: "image/png" });
-
-//       // Create a download link for the PNG image
-//       const downloadLink = document.createElement("a");
-//       downloadLink.href = URL.createObjectURL(file);
-//       downloadLink.download = "Poster.png";
-
-//       // Trigger the download
-//       downloadLink.click();
-
-//       // Release the object URLs
-//       URL.revokeObjectURL(url);
-//       URL.revokeObjectURL(downloadLink.href);
-//     }, "image/png");
-//   };
-//   console.log("clicked");
-//   imageData.src = url;
-// });
+  canvas.toBlob(function (blob) {
+    const file = new File([blob], "Poster.png", { type: "image/png" });
+    const downloadLink = document.createElement("a");
+    downloadLink.href = URL.createObjectURL(file);
+    downloadLink.download = "Poster.png";
+    downloadLink.click();
+  }, "image/png");
+});
